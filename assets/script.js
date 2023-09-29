17038f288c377bd54950d3a2ee3699337a63ab06
David Andaman <david.andaman@gmail.com>
2023-09-29 15:51:54 +0200

    Slide show opérationnel

--- assets/script.js
+++ assets/script.js
@@ -12,37 +12,62 @@ slideBoutonRight.addEventListener("click", function () {
     let pathImage = ("/assets/images/slideshow/")
  // Tableau contenant les éléments avec des clés et des valeurs
 	let diapo = [
-		{ image: pathImage + "slide1.jpg", tagLine: "Impressions tous formats <span>en boutique et en ligne</span>", bullet : "x" },
-		{ image: pathImage + "slide2.jpg", tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>", bullet : "x" },
-		{ image: pathImage + "slide3.jpg", tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>", bullet : "x" },
-		{ image: pathImage + "slide4.png", tagLine: "Autocollants <span>avec découpe laser sur mesure</span>", bullet : "x" },
+		{ image: pathImage + "slide1.jpg", tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",},
+		{ image: pathImage + "slide2.jpg", tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"},
+		{ image: pathImage + "slide3.jpg", tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",},
+		{ image: pathImage + "slide4.png", tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"},
 		// Ajoutez d'autres éléments avec leurs clés et valeurs ici
 	  ];
 	
-    // Fonction pour générer le contenu HTML
-    function genererContenu(slides) {
-      let contenu = "";
 
-      for (let i = 0; i < diapo.length; i++) {
-        let element = diapo[i];
-        let image = element.image;
-        let tagLine = element.tagLine;
+	  function genererContenu(slides) {
+		let contenu = "";
+		let dots = "";
+	  
+		for (let i = 0; i < diapo.length; i++) {
+		  let element = diapo[i];
+		  let image = element.image;
+		  let tagLine = element.tagLine;
+		  let bullet = element.bullet;
+	  
+		  // Générer le contenu HTML pour chaque élément
+		  contenu += "<div class='slider_content_item'>";
+		  contenu += "<img src='" + image + "' class='image'>";
+		  contenu += "<p class='tag-line'>" + tagLine + "</p>";
+		  contenu += "</div>";
+	  
+		  // Générer le contenu HTML pour chaque dot
+		  dots += "<span class='dot " + (i === 0 ? "dot_selected" : "") + "' data-index='" + i + "'></span>";
+		}
+	  
+		// Ajouter les dots au contenu HTML
+		contenu += "<div class='dots'>" + dots + "</div>";
+	  
+		return contenu;
+	  }
 
-        // Générer le contenu HTML pour chaque élément
-        contenu += "<div class='slider_content_item'>";
-        contenu += "<img src='" + image + "' class='image'>";
-        contenu += "<p class='tag-line'>" + tagLine + "</p>";
-        contenu += "</div>";
-      }
+// Obtenir une référence à l'élément cible
+let divElement = document.querySelector(".slider_content");
 
-      return contenu;
-    }
+// Générer le contenu HTML et l'insérer dans l'élément cible
+divElement.innerHTML = genererContenu(diapo);
 
-    // Obtenir une référence à l'élément cible
-    let divElement = document.querySelector(".slider_content");
+// Obtenir une référence aux éléments dots
+let dotsElements = document.querySelectorAll(".dot");
 
-    // Générer le contenu HTML et l'insérer dans l'élément cible
-    divElement.innerHTML = genererContenu(diapo);
+// Gestionnaire d'événement pour le scrollLeft de l'élément cible
+divElement.addEventListener("scroll", function () {
+  // Obtenir la position actuelle de la slide
+  let slidePosition = Math.round(this.scrollLeft / this.clientWidth);
+
+  // Mettre à jour la classe des dots en fonction de la position de la slide
+  dotsElements.forEach((dot, index) => {
+    dot.classList.toggle("dot_selected", index === slidePosition);
+  });
+});
+
+
+
 
 
 
@@ -72,10 +97,4 @@ slideBoutonRight.addEventListener("click", function () {
 				sliderContent.scrollLeft = 0;
 			}
 		}
-
-
-
-
-
-
 
